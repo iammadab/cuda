5507dffeb5381ef2991df4fce6578d0c3cf88495
Wisdom Ogwu <iammadab@gmail.com>
2025-10-13 19:21:12 +0100

    generated grayscale output

--- grayscale.cu
+++ grayscale.cu
@@ -6,22 +6,59 @@
 
 #include <stdio.h>
 
+// TODO: check_err
+
+__global__ void rgb_to_grayscale_kernel(unsigned char *in, unsigned char *out, size_t pixels) {
+  size_t id = blockIdx.x * blockDim.x + threadIdx.x;
+  
+  if (id < pixels) {
+    // grayscale computation
+    // g = 0.299 * r + 0.587 * g + 0.114 * b;
+    size_t offset = id * 3;
+    
+    float r = (float) in[offset];
+    float g = (float) in[offset + 1];
+    float b = (float) in[offset + 2];
+
+    float y = 0.299f * r + 0.587f * g + 0.114f * b;
+
+    out[id] = (unsigned char) (y + 0.5f);
+  }
+}
 
 int main () {
-  int desired_channels = 3;
+  int desired_channels = 3; // rgb
   int width, height, channels;
 
+  // load img
   unsigned char *img_data = stbi_load("images/sheeps.jpg", &width, &height, &channels, desired_channels);
   if (!img_data) {
     fprintf(stderr, "Failed to load image: %s", stbi_failure_reason());
     return 1;
   }
 
-  size_t total_img_bytes = (size_t)width * height * channels;
+  size_t pixels = width * height;
+  size_t total_img_bytes = pixels * desired_channels;
 
-  // write img to a file
+  // allocate GPU memory for input image and grayscale output
+  unsigned char *img_d, *grayscale_d;
+  cudaMalloc(&img_d, total_img_bytes * sizeof(unsigned char));
+  cudaMalloc(&grayscale_d, pixels * sizeof(unsigned char));
+
+  // copy image data to the GPU
+  cudaMemcpy(img_d, img_data, total_img_bytes, cudaMemcpyHostToDevice);
+
+  int block = 256;
+  int grid = ceil(pixels / 256.0);
+  rgb_to_grayscale_kernel<<<grid, block>>>(img_d, grayscale_d, pixels);
+
+  // copy grayscale data from the gpu
+  unsigned char *grayscale_h = (unsigned char *) malloc(pixels);
+  cudaMemcpy(grayscale_h, grayscale_d, pixels, cudaMemcpyDeviceToHost);
+
+  // write grayscale to file
   int stride = width * desired_channels; // tightly packed, no data alignment
-  if (!stbi_write_png("output.png", width, height, desired_channels, img_data, width * desired_channels)) {
+  if (!stbi_write_png("images/sheeps_grayscale.png", width, height, 1, grayscale_h, width)) {
       fprintf(stderr, "Write failed!\n");
       stbi_image_free(img_data);
       return 1;
