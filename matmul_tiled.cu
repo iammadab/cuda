6ab7d80878d7219c3d01f3abea365a7f90e7f56d
Wisdom Ogwu <iammadab@gmail.com>
2025-10-26 18:46:40 +0100

    implement tiling kernel

--- matmul_tiled.cu
+++ matmul_tiled.cu
@@ -13,14 +13,36 @@ int N = 100;
 // B = (K, N)
 // C = (M, N)
 
-__global__ void matmul_kernel_tiled(float *A, float *B, float *C, int M, int N, int k) {}
+// TODO: plot improvement with different tile widths
+
+#define TILE_WIDTH 16
+
+// TODO: handle irregular sized tile_widths
+
+__global__ void matmul_kernel_tiled(float *A, float *B, float *C, int K) {
+  int row = blockIdx.y * blockDim.y + threadIdx.y;
+  int col = blockIdx.x * blockDim.x + threadIdx.x;
+
+  __shared__ float Ads[TILE_WIDTH][TILE_WIDTH];
+  __shared__ float Bds[TILE_WIDTH][TILE_WIDTH];
+
+  float sum = 0;
+
+  for (int phase = 0; phase < K / TILE_WIDTH; ++phase) {
+    Ads[row][col] = A[row * K + (phase * TILE_WIDTH + threadIdx.x)];
+    Bds[row][col] = B[(phase * TILE_WIDTH + threadIdx.y) * K + col];
+    __syncthreads();
+
+    for (int i = 0; i < TILE_WIDTH; ++i) {
+      sum += Ads[row][i] * Bds[i][col];
+    }
+    __syncthreads();
+  }
+
+  C[row * K + col] = sum;
+}
 
 int main() {
-  // allocate memory on the cpu and the gpu
-  // do warmup
-  // run kernel + timing
-  // then do correctness check
-
   int size_a = M * K;
   int size_b = K * N;
   int size_c = M * N;
@@ -37,8 +59,13 @@ int main() {
   check_err(cudaMalloc(&B_d, size_b * sizeof(float)));
   check_err(cudaMalloc(&C_d, size_c * sizeof(float)));
 
+  // TODO: move the data to the GPU
+
   // compute expected answer on the cpu
-  matmul_cpu(A_h, B_h, C_h_cpu_result, M, N, K);
+  matmul_cpu(A_h, B_h, C_h_cpu_result, K);
+
+  // TODO: use the tile_width as the block size
+  // TODO: compute the GRID size accordingly
 
   return 0;
 }
