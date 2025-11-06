9376a2a8117410f421f0fab13d4b3ca0a5a0a391
Wisdom Ogwu <iammadab@gmail.com>
2025-11-06 19:25:28 +0100

    make a filter_width a function over filter_size

--- kernels/convolution/conv_basic.cu
+++ kernels/convolution/conv_basic.cu
@@ -1,8 +1,9 @@
 # define LINALG_IMPLEMENTATION
 # include "../../include/linalg.h"
 
-# define N 5
-# define FILTER_SIZE 3
+int N = 5;
+int FILTER_SIZE = 1;
+int FILTER_WIDTH = 2 * FILTER_SIZE + 1;
 
 int main() {
   // allocate memory for the input
@@ -15,32 +16,27 @@ int main() {
   print_arr_2d(A, N, size);
 
   // allocate memory for the filter
-  int size_f = FILTER_SIZE * FILTER_SIZE; 
+  int size_f = FILTER_WIDTH * FILTER_WIDTH; 
   int size_f_bytes = size_f * sizeof(float);
   float *F = (float*) malloc(size_f_bytes);
-  F[(FILTER_SIZE / 2) * FILTER_SIZE + (FILTER_SIZE / 2)] = 2.0f;
-  print_arr_2d(F, FILTER_SIZE, size_f);
+  // scale each element by a factor
+  F[FILTER_SIZE * FILTER_WIDTH + FILTER_SIZE] = 2.0;
+  print_arr_2d(F, FILTER_WIDTH, size_f);
 
   // allocate the output
   float *C = (float*) malloc(size_bytes);
-  print_arr_2d(C, N, size);
 
   // perform convolution
   for (int row = 0; row < N; ++row) {
     for (int col = 0; col < N; ++col) {
-      // the above loop will iterate over all the threads in the kernel
-      // we need each of them to compute their convolution
-      // we can use the blur size
-      // we can start at -FILTER_SIZE to FILTER_SIZE
-
       float sum = 0.0;
 
       // TODO make this cleaner
-      for (int frow = 0; frow < FILTER_SIZE; ++frow) {
-        for (int fcol = 0; fcol < FILTER_SIZE; ++fcol) {
+      for (int frow = 0; frow < FILTER_WIDTH; ++frow) {
+        for (int fcol = 0; fcol < FILTER_WIDTH; ++fcol) {
           int nrow = row + frow - 1; 
           int ncol = col + fcol - 1; 
-          sum += A[nrow * N + ncol] * F[frow * FILTER_SIZE + fcol]; 
+          sum += A[nrow * N + ncol] * F[frow * FILTER_WIDTH + fcol]; 
         }
       }
       C[row * N + col] = sum;
